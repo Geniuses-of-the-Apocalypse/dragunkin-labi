c5f2e77453a04c2dfa4522b5e337dccf5b9e1510
Wведэшка <f.fa.66@mail.ru>
2026-02-28 18:29:59 +0300

    Update main.java

--- java/lab4(3)/main.java
+++ java/lab4(3)/main.java
@@ -1,5 +1,6 @@
 package lab3java;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== ТЕСТ 1: Decimal ===");
@@ -8,9 +9,9 @@ public class Main {
         Decimal d3 = new Decimal("-50");
         Decimal d4 = new Decimal("30");
 
-        System.out.print("123+456=");
+        System.out.print("123 + 456 = ");
         d1.add(d2).print();
-        System.out.print("-50+30=");
+        System.out.print("-50 + 30 = ");
         d3.add(d4).print();
 
         System.out.println("\n=== ТЕСТ 2: Hex ===");
@@ -19,9 +20,9 @@ public class Main {
         Hex h3 = new Hex("FFF");
         Hex h4 = new Hex("1");
 
-        System.out.print("A5+5A=");
+        System.out.print("A5 + 5A = ");
         h1.add(h2).print();
-        System.out.print("FFF+1=");
+        System.out.print("FFF + 1 = ");
         h3.add(h4).print();
 
         System.out.println("\n=== ТЕСТ 3: Интерфейс ===");
@@ -30,9 +31,9 @@ public class Main {
         Array a3 = new Hex("ABC");
         Array a4 = new Hex("123");
 
-        System.out.print("777+222=");
+        System.out.print("777 + 222 = ");
         a1.add(a2).print();
-        System.out.print("ABC+123=");
+        System.out.print("ABC + 123 = ");
         a3.add(a4).print();
     }
 }
