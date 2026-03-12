bf7de66f961376d5bf822806a3607d4f7dba5c0a
Wведэшка <f.fa.66@mail.ru>
2026-03-13 00:27:35 +0300

    Create inter.java

---
+++ java/lab 4(4)/inter.java
@@ -0,0 +1,10 @@
+package lab4java;
+
+public interface inter {
+    inter men( inter other);
+    inter menr( inter other);
+    inter raf(inter other);
+    inter bol(inter other);
+    inter bolr(inter other);
+    inter suur();
+}
