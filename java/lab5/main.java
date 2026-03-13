579b37ea69d143d05d7cbfbed877af823dc35ced
Wведэшка <f.fa.66@mail.ru>
2026-03-14 01:40:19 +0300

    Create main.java

---
+++ java/lab5/main.java
@@ -0,0 +1,8 @@
+package lab005java;
+public class main {
+    public static void main(String[] args) {
+        String text = "Hello world! Hello, WORLD. This is a test text. I am caterpillar";
+        int result = poisk.poisk1(text);
+        System.out.println("Количество уникальных слов: " + result);
+        String text2 = "Java java JAVA Python python";
+        System.out.println("Уникальных слов во втором тексте: " + poisk.poisk1(text2));}}
