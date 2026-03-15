aa8e1bb1d89815a1ab01fb3aad2cf393ef11bfb2
Wведэшка <f.fa.66@mail.ru>
2026-03-16 00:01:28 +0300

    Update poisk.java

--- java/lab5/poisk.java
+++ java/lab5/poisk.java
@@ -1,6 +1,7 @@
 package lab005java;
 import java.util.*;
 import java.util.regex.*;
+import java.util.Scanner;
 public class poisk {
     public static int poisk1(String text) {
         Pattern pattern = Pattern.compile("\\b\\w+\\b");
@@ -9,4 +10,12 @@ public class poisk {
         while (matcher.find()) {
             String word = matcher.group().toLowerCase();
             uniqueWords.add(word);}
-        return uniqueWords.size();}}
+        int a= uniqueWords.size();
+        System.out.println(a);
+        return a;}
+    public static String vvod(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите строку");
+        String text = scanner.nextLine();
+        System.out.println("вы ввели " + text);
+        return text;}}
