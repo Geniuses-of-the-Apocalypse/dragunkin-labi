d25ab63179b07b998eab566ab6ba514f22345b17
Wведэшка <f.fa.66@mail.ru>
2026-03-16 00:01:54 +0300

    Update main.java

--- java/lab5/main.java
+++ java/lab5/main.java
@@ -1,8 +1,9 @@
 package lab005java;
 public class main {
-    public static void main(String[] args) {
-        String text = "Hello world! Hello, WORLD. This is a test text. I am caterpillar";
-        int result = poisk.poisk1(text);
-        System.out.println("Количество уникальных слов: " + result);
-        String text2 = "Java java JAVA Python python";
-        System.out.println("Уникальных слов во втором тексте: " + poisk.poisk1(text2));}}
+        public static void main(String[] args) {
+            String text = "Hello world! Hello, WORLD. This is a test text. I am caterpillar";
+            int result = poisk.poisk1(text);
+            System.out.println("Количество уникальных слов: " + result);
+            String text2 = "Java java JAVA Python python";
+            System.out.println("Уникальных слов во втором тексте: " + poisk.poisk1(text2));
+            poisk t= new poisk(); poisk a = new poisk(); a.poisk1(t.vvod());}}
